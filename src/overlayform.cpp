18f762648dbfb836b99ea9c6233fbd47e2c9ba43
James Rudoplph <jrudo004@odu.edu>
2020-10-30 17:38:24 -0400

    add overlay + toggle overlay

--- src/overlayform.cpp
+++ src/overlayform.cpp
@@ -15,17 +15,52 @@ OverlayForm::~OverlayForm()
 }
 
 void OverlayForm::displayImage() {
-    QGraphicsScene *scene = new QGraphicsScene();
     QPixmap pixmap = QPixmap::fromImage(QImage::fromData(imageData));
 
-    scene->addPixmap(pixmap);
-    ui->graphicsView->setScene(scene);
+    baseScene = new QGraphicsScene();
+    baseScene->addPixmap(pixmap);
+
+    ui->graphicsView->setScene(baseScene);
+}
+
+void OverlayForm::displayOverlay() {
+    QPixmap pixmapBase = QPixmap::fromImage(QImage::fromData(imageData));
+    QImage overlay = QImage::fromData(overlayData);
+    overlay = overlay.convertToFormat(QImage::Format_ARGB32);
+
+    for (int i = 0; i < overlay.height(); i++) {
+        for (int j = 0; j < overlay.width(); j++) {
+            if (overlay.pixelColor(i, j).rgb() == 0xffffffff) {
+                overlay.setPixel(i, j, 0x00000000);
+            }
+        }
+    }
+
+    QPixmap pixmapOverlay = QPixmap::fromImage(overlay);
+
+    overlaidScene = new QGraphicsScene();
+    overlaidScene->addPixmap(pixmapBase);
+    overlaidScene->addPixmap(pixmapOverlay);
+
+    ui->graphicsView->setScene(overlaidScene);
+}
+
+void OverlayForm::toggleOverlay() {
+    if (overlaidScene != nullptr) {
+        if (overlayShowing) {
+            overlayShowing = false;
+            ui->graphicsView->setScene(baseScene);
+        } else {
+            overlayShowing = true;
+            ui->graphicsView->setScene(overlaidScene);
+        }
+    }
 }
 
 //refactor everything below this to a server class later for reuse in b/c
 void OverlayForm::initSocket() {
     udpSocket = new QUdpSocket(this);
-    udpSocket->bind(QHostAddress::AnyIPv4, 420);
+    udpSocket->bind(QHostAddress::LocalHost, 420);
 
     connect(udpSocket, &QUdpSocket::readyRead, this, &OverlayForm::readPendingDatagrams);
 }
@@ -52,9 +87,24 @@ void OverlayForm::processDatagram(QByteArray datagram) {
         qDebug() << "received END_IMAGE_SEND";
         displayImage();
         receivingImage = false;
-    } else if (receivingImage) {
+    } else if (datagram.data() == QStringLiteral("START_OVERLAY_SEND")) {
+        qDebug() << "received START_OVERLAY_SEND";
+        overlayData.clear();
+        receivingOverlay = true;
+    } else if (datagram.data() == QStringLiteral("END_OVERLAY_SEND")) {
+        qDebug() << "received END_OVERLAY_SEND";
+        displayOverlay();
+        receivingOverlay = false;
+    } else if (datagram.data() == QStringLiteral("TOGGLE_OVERLAY")) {
+        qDebug() << "received TOGGLE_OVERLAY";
+        toggleOverlay();
+    }else if (receivingImage) {
         qDebug() << "received " << datagram.length() << " bytes of image data";
         imageData.append(datagram);
-        return;
+    } else if (receivingOverlay) {
+        qDebug() << "received " << datagram.length() << " bytes of overlay data";
+        overlayData.append(datagram);
+    } else {
+        qDebug() << datagram;
     }
 }
--- src/overlayform.h
+++ src/overlayform.h
@@ -1,6 +1,7 @@
 #ifndef OVERLAYFORM_H
 #define OVERLAYFORM_H
 
+#include <QGraphicsScene>
 #include <QUdpSocket>
 #include <QWidget>
 
@@ -16,15 +17,24 @@ public:
     explicit OverlayForm(QWidget *parent = nullptr);
     ~OverlayForm();
 
+private slots:
+
 private:
     Ui::OverlayForm *ui;
-    QUdpSocket *udpSocket;
+    QUdpSocket *udpSocket = nullptr;
+    QGraphicsScene *baseScene = nullptr;
+    QGraphicsScene *overlaidScene = nullptr;
     QByteArray imageData;
+    QByteArray overlayData;
+    bool overlayShowing =  false;
     bool receivingImage = false;
+    bool receivingOverlay = false;
     void initSocket();
     void readPendingDatagrams();
     void processDatagram(QByteArray datagram);
     void displayImage();
+    void toggleOverlay();
+    void displayOverlay();
 };
 
 #endif // OVERLAYFORM_H
